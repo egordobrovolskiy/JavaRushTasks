0fbb2b5d087b2ed834d4471a2bfb372252e772d1
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-15 17:57:11 +0300

    Решил Рефакторинг (14)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -5,9 +5,14 @@ public class User {
     private String surname;
     private int age;
 
-    private String country;
-    private String city;
-    private House house;
+    private boolean man;
+
+    private Address address;
+
+
+//    private String country;
+//    private String city;
+//    private House house;
 
     private Work work;
 
@@ -27,6 +32,10 @@ public class User {
             System.out.println("Пользователь моложе 16 лет");
         else
             System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss() {
+       return work.getBoss();
     }
 
     public String getName() {
@@ -53,25 +62,6 @@ public class User {
         this.age = age;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return country + " " + city + " " + house.house;
-    }
 
     public Work getWork() {
         return work;
@@ -79,5 +69,31 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+    public String getAddress() {
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
+    }
+    public String getCountry() {
+        return address.getCountry();
+    }
+
+    public void setCountry(String country) {
+        address.setCountry(country);
+    }
+
+    public String getCity() {
+        return address.getCity();
+    }
+
+    public void setCity(String city) {
+        address.setCity(city);
     }
 }
