4019e76ac5dbdfae3895ef23e5aef338506448ca
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-18 12:26:23 +0300

    Чат (7)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,23 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+        Map<String, Connection> map = getConnectionMap();
+        for (Map.Entry<String, Connection> pair: map.entrySet()) {
+            try {
+                pair.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
     private static class Handler extends Thread {
         Socket socket;
         public Handler(Socket socket) {
@@ -33,5 +48,13 @@ public class Server {
 
         }
 
+    }
+
+    public static Map<String, Connection> getConnectionMap() {
+        return connectionMap;
+    }
+
+    public static void setConnectionMap(Map<String, Connection> connectionMap) {
+        Server.connectionMap = connectionMap;
     }
 }
