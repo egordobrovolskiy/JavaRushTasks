72b598c63f4eec54b1afae2f5ac8ae0e03902ff1
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-19 13:59:19 +0300

    Archiver (8) - выбор параметра

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,15 +11,32 @@ import java.nio.file.Paths;
 
 public class Archiver {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Укажите путь и имя нового архива");
-        String zipPath = reader.readLine();
-        ZipFileManager zipFile = new ZipFileManager(Paths.get(zipPath));
-        System.out.println("Укажите путь и имя файла, который будем архивировать");
-        String filePath = reader.readLine();
-        zipFile.createZip(Paths.get(filePath));
+    public static void main(String[] args) {
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        new ExitCommand().execute();
+        } while (operation != Operation.EXIT);
+    }
+
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage("0 - упаковать файлы в архив");
+        ConsoleHelper.writeMessage("1 - добавить файл в архив");
+        ConsoleHelper.writeMessage("2 - удалить файл из архива");
+        ConsoleHelper.writeMessage("3 - распаковать архив");
+        ConsoleHelper.writeMessage("4 - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage("5 – выход");
+        int command = ConsoleHelper.readInt();
+        return Operation.values()[command]; //вызываем enum согласно полученному номеру
     }
 }
+
