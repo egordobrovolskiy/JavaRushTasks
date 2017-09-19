586c9b9df2292fc9ff7aa714766ff93db6fe2444
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-19 18:27:35 +0300

    Archiver (15) Накодили расспаковку архива.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task31.task3110.command;
 
-
 import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.FileProperties;
 import com.javarush.task.task31.task3110.ZipFileManager;
@@ -11,12 +10,16 @@ public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
         ZipFileManager zipFileManager = getZipFileManager();
+
         ConsoleHelper.writeMessage("Содержимое архива:");
-        List<FileProperties> list = zipFileManager.getFilesList();
-        for (FileProperties file : list) {
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
             ConsoleHelper.writeMessage(file.toString());
         }
+
         ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
