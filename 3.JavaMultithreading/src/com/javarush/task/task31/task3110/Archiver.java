d41e8e542acc4fab7e6917ea26a3d531cb5c3d4d
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-19 10:44:18 +0300

    Archiver (1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Укажите путь и имя нового архива");
+        String zipPath = reader.readLine();
+        ZipFileManager zipFile = new ZipFileManager(Paths.get(zipPath));
+        System.out.println("Укажите путь и имя файла, который будем архивировать");
+        String filePath = reader.readLine();
+        zipFile.createZip(Paths.get(filePath));
+
+    }
+}
