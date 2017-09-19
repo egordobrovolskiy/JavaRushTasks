e92570113e7405d7b472c1fad1827cb64ec21cee
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-19 11:21:18 +0300

    Archiver (2) - уже архивирует :)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
 
@@ -11,6 +15,17 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOut = new ZipOutputStream(Files.newOutputStream(zipFile))) {
 
-    }
+        ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+        zipOut.putNextEntry(zipEntry);
+
+        try (InputStream inputStream = Files.newInputStream(source)) {
+        while (inputStream.available() > 0) {
+            zipOut.write(inputStream.read());
+        }
+        }
+        zipOut.closeEntry();
+
+    }}
 }
