1d084c01d65285892140f1caca566d3201b9dc8c
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-19 12:53:04 +0300

    Archiver (4) - начало добавления команд.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,5 +18,6 @@ public class Archiver {
         String filePath = reader.readLine();
         zipFile.createZip(Paths.get(filePath));
 
+        new ExitCommand().execute();
     }
 }
