ce7a385465e3b680e31232496a17e24c5e1f2efc
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-21 13:29:20 +0300

    HTML Editor (10)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -23,7 +23,12 @@ public class View extends JFrame implements ActionListener {
         }
     }
 
-
+    public boolean canUndo() {
+        return false;
+    }
+    public boolean canRedo() {
+        return false;
+    }
 
     public void initMenuBar() {
 
