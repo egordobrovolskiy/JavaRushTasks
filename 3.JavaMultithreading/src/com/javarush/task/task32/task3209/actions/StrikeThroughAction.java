a4365e0e4e4fa2af1b9d99c89e674adcc0e81b02
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-21 12:49:06 +0300

    HTML Editor (8) - что произходит?

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/StrikeThroughAction.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task32.task3209.actions;
+
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledEditorKit;
+import java.awt.event.ActionEvent;
+
+public class StrikeThroughAction extends StyledEditorKit.StyledTextAction {
+
+    public StrikeThroughAction() {
+        super(StyleConstants.Subscript.toString());
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
