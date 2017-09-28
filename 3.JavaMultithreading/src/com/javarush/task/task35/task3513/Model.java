49672ef5e6734d38dcbae1ceddd85d25840c7a83
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-09-28 18:06:29 +0300

    2048 (3) - Создали и заполнили игровое поле.

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,16 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private  static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model () {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int k = 0; k < gameTiles.length; k++) {
+                gameTiles[i][k] = new Tile();
+            }
+        }
+    }
+
 }
