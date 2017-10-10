cc854e97eedd645945233113dbe74047c7081437
egordobrovolskiy <egordobrovolskiy@gmail.com>
2017-10-10 17:27:56 +0300

    1. Освоен паттерн Factory Method. 2. Освоен паттерн Abstract Factory.

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task37.task3702;
+
+import com.javarush.task.task37.task3702.female.FemaleFactory;
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
+public class FactoryProducer {
+    public static enum HumanFactoryType {
+        MALE,
+        FEMALE
+    }
+
+    public static AbstractFactory getFactory(HumanFactoryType factoryType) {
+        if (factoryType == HumanFactoryType.MALE) return new MaleFactory();
+        else if (factoryType == HumanFactoryType.FEMALE) return new FemaleFactory();
+        else return null;
+    }
+}
