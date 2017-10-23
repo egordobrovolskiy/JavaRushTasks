04a2a5f20471c8cd43fc81e079e6c1d08517b509
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-10-23 16:29:06 +0300

    Aggregator (4)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task28.task2810.model;
 
 public class HHStrategy implements Strategy {
+    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
 
+//    String url = String.format(URL_FORMAT, "Kiev", 3);
 }
