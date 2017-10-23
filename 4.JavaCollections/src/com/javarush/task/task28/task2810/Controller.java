fee0683cf8cd9e8b836461617696fd895e1870e6
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-10-23 21:50:40 +0300

    Aggregator (11)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
@@ -8,29 +9,41 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
-    private Provider[] providers;
+//    private Provider[] providers;
+    private Model model;
 
-    public Controller (Provider... providers) throws IllegalArgumentException {
-        if (providers == null || providers.length == 0) throw new IllegalArgumentException();
-        this.providers = providers;
-    }
+//    public Controller (Provider... providers) throws IllegalArgumentException {
+//        if (providers == null || providers.length == 0) throw new IllegalArgumentException();
+//        this.providers = providers;
+//    }
 
-
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> list = new ArrayList<>();
-        for (int i=0;i<providers.length;i++){
-            for (Vacancy vacancy : providers[i].getJavaVacancies("")){
-                list.add(vacancy);
-            }
+    public Controller(Model model) throws IllegalArgumentException {
+        if (model == null) {
+            throw new IllegalArgumentException("Illegal arguments");
         }
-        System.out.println(list.size());
+        this.model = model;
     }
+
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
+    }
+
+
+//    @Override
+//    public String toString() {
+//        return "Controller{" +
+//                "providers=" + Arrays.toString(providers) +
+//                '}';
+//    }
+
+//    public void scan() {
+//        List<Vacancy> list = new ArrayList<>();
+//        for (int i=0;i<providers.length;i++){
+//            for (Vacancy vacancy : providers[i].getJavaVacancies("")){
+//                list.add(vacancy);
+//            }
+//        }
+//        System.out.println(list.size());
+//    }
 }
 
